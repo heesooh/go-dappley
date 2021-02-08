b9e61e34a41159abc1f26c230f70ad2e3bc7805a
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-02-08 14:44:07 -0800

    Update sendEmail.go
    - comments added
    - new attachment file change.txt created
    - updated the message setting so the email displays the information about the commit

--- sendEmail.go
+++ sendEmail.go
@@ -20,28 +20,48 @@ func main() {
 }
 
 func sendEmail(fileName string, commitInfo string, commitEmail string) {
+	//read log file
 	errMSG, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return
 	}
+	//read commitInfo file
+	commitMSG, err := ioutil.ReadFile(commitInfo)
+	if err != nil {
+		return
+	}
+	//convert to string
 	ERROR := string(errMSG)
+	COMMIT := string(commitMSG)
+
 	if (strings.Contains(ERROR, "FAIL")) {
-		errorEmail := commitInfo + commitEmail + "<p> Failing Tests: </p>"
-		scanner := bufio.NewScanner(strings.NewReader(ERROR))
-		for scanner.Scan() {
-			MSG := scanner.Text()
+		//create the html formatted email
+		errorEmail := "<p> Failing Tests:"
+		CommitScanner := bufio.NewScanner(strings.NewReader(COMMIT))
+		for CommitScanner.Scan() {
+			MSG := CommitScanner.Text()
+			MSG = "<br>" + MSG
+			errorEmail += MSG
+		}
+		errorEmail += "</p> <p>"
+		ErrorScanner := bufio.NewScanner(strings.NewReader(ERROR))
+		for ErrorScanner.Scan() {
+			MSG := ErrorScanner.Text()
 			if (strings.Contains(MSG, "FAIL")) {
-				MSG = "<p>" + MSG + "</p>"
-				errorEmail = errorEmail + MSG
+				MSG = "<br>" + MSG
+				errorEmail += MSG
 			}
 		}
+		errorEmail += "</p>"
+		//send the email
 		m := gomail.NewMessage()
 		m.SetHeader("From", "blockchainwarning@omnisolu.com")
-		m.SetHeader("To", "blockchainwarning@omnisolu.com", "blockchainwarning@omnisolu.com")
+		m.SetHeader("To", "blockchainwarning@omnisolu.com")
 		//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
-		m.SetHeader("Subject", "Dappley Error Report:")
+		m.SetHeader("Subject", "go-dappley Error Report:")
 		m.SetBody("text/html", errorEmail)
 		m.Attach(fileName)
+		m.Attach("change.txt")
 
 		d := gomail.NewDialer("smtp.gmail.com", 587, "blockchainwarning@omnisolu.com", "01353751")
 
